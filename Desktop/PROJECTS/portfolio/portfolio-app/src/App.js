5671cdb5cdab2a63764d86b7515d005749f18f0d
Ben Guo <benguo@Bens-MacBook-Pro.local>
2022-09-09 18:00:19 -0700

    fix

--- Desktop/PROJECTS/portfolio/portfolio-app/src/App.js
+++ Desktop/PROJECTS/portfolio/portfolio-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          will edit
+          will edit later
         </p>
         <a
           className="App-link"
