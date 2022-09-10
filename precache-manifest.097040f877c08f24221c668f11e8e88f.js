f083382b7192f6134b80ddb7564a0a156f4dfee5
Ben Guo <benguo@Bens-MacBook-Pro.local>
2022-09-09 17:51:49 -0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.097040f877c08f24221c668f11e8e88f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "248036d992633f18ec6d",
+    "url": "/static/css/main.8e61da7f.chunk.css"
+  },
+  {
+    "revision": "248036d992633f18ec6d",
+    "url": "/static/js/main.248036d9.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9094fb0a2429991bd90dce3a3d6fd443",
+    "url": "/index.html"
+  }
+];
